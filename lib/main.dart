43e635f4b0d68de5a167ac1266aa1905de00e44b
Jonathanfilbert <jonathan.lisyanto@bpkpenabur.sch.id>
2019-12-24 01:55:22 +0700

    UI done

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,49 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(Main());
+void main() => runApp(MaterialApp(
+      home: App(),
+    ));
 
-class Main extends StatelessWidget {
-  const Main({Key key}) : super(key: key);
-
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: SafeArea(
-          child: Container(
-        width: double.infinity,
-        height: double.infinity,
-        color: Colors.green,
-        child: Padding(
-          padding: EdgeInsets.symmetric(vertical: 20.0),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Expanded(
-                child: Container(
-                  color: Colors.red,
+    return Scaffold(
+      backgroundColor: Colors.red,
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Dice App"),
+        backgroundColor: Colors.red,
+      ),
+      body: Flex(
+          direction: Axis.vertical,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Image(
+                      image: AssetImage("assets/images/dice1.png"),
+                    ),
+                  ),
                 ),
-              ),
-              Expanded(
-                  flex: 2,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Container(
-                        width: 100.0,
-                        height: 100.0,
-                        color: Colors.yellow,
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: GestureDetector(
+                      onTap: () {
+                        print("Tapped");
+                      },
+                      child: Image(
+                        image: AssetImage("assets/images/dice2.png"),
                       ),
-                      Container(
-                        width: 100.0,
-                        height: 100.0,
-                        color: Colors.green[200],
-                      )
-                    ],
-                  )),
-              Expanded(
-                child: Container(
-                  color: Colors.blue,
+                    ),
+                  ),
                 ),
-              )
-            ],
-          ),
-        ),
-      )),
+              ],
+            )
+          ]),
     );
   }
 }
