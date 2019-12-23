356e17518938469611763a7d6d615947004cab87
Jonathanfilbert <jonathan.lisyanto@bpkpenabur.sch.id>
2019-12-24 02:26:26 +0700

    dark mode & logic implemented, DONE

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,95 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(MaterialApp(
       home: App(),
     ));
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  int diceOne = 1;
+  int diceTwo = 1;
+  _handleClick() {
+    setState(() {
+      Random random = new Random();
+      diceTwo = random.nextInt(6) + 1;
+      diceOne = random.nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.red,
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text("Dice App"),
-        backgroundColor: Colors.red,
-      ),
-      body: Flex(
-          direction: Axis.vertical,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Image(
-                      image: AssetImage("assets/images/dice1.png"),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: GestureDetector(
-                      onTap: () {
-                        print("Tapped");
-                      },
-                      child: Image(
-                        image: AssetImage("assets/images/dice2.png"),
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Flex(
+            direction: Axis.vertical,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(left: 16.0),
+                child: Flex(
+                    direction: Axis.horizontal,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          Text(
+                            "Dice",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                              fontSize: 40.0,
+                            ),
+                          ),
+                        ],
+                      )
+                    ]),
+              ),
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: GestureDetector(
+                        onTap: () {
+                          _handleClick();
+                        },
+                        child: Image(
+                          image: AssetImage("assets/images/dice$diceOne.png"),
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
-            )
-          ]),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: GestureDetector(
+                        onTap: () {
+                          _handleClick();
+                        },
+                        child: Image(
+                          image: AssetImage('assets/images/dice$diceTwo.png'),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                width: double.infinity,
+                height: 60.0,
+              ),
+              Text(
+                "The total number is ${diceOne + diceTwo}",
+                style: TextStyle(fontSize: 30.0, color: Colors.white),
+              ),
+            ]),
+      ),
     );
   }
 }
